57936b13fea6dbc6210d10d8a88ad90fc04a0242
Guklam <anonyguk@yahoo.fr>
2018-06-13 14:50:40 +0200

    Modify files

--- includes/Footer.php
+++ includes/Footer.php
@@ -1,3 +1,34 @@
 <div id="footer">
-    Test
+    <div class="footer-image">
+        <img src="images/logo-echinghen.png">
+        <p>Mairie, Route de Baincthun</p>
+        <p>62360 Echinghen</p>
+    </div>
+    <div class="footer-menu">
+        <p>Lien utiles</p>
+        <ul>
+            <li><a href="#"><i class="fa fa-angle-right"></i> La CAB</a></li>
+            <li><a href="#"><i class="fa fa-angle-right"></i> Google</a></li>
+            <li><a href="#"><i class="fa fa-angle-right"></i> Test</a></li>
+        </ul>
+    </div>
+    <div class="footer-contact">
+        <p><i class="fas fa-phone"></i><span class="bold">03.21.91.17.67</span></p>
+        <p><i class="fas fa-fax"></i><span class="bold">03.21.33.86.57</span></p>
+        <p><i class="fas fa-envelope"></i><a href="mailto:mairie.echinghen@orange.fr">mairie.echinghen@orange.fr</a></p>
+        <p><i class="fas fa-address-book"></i><a href="#">Nous contacter</a></p>
+    </div>
+    <div class="footer-newsletter">
+        <p>Inscrivez-vous à la newsletter pour ne plus rien louper de l'actualité!</p>
+        <div class="input-group mb-3">
+        <div class="input-icon">
+            <span><i class="fas fa-envelope"></i></span>
+        </div>
+        <input type="text" class="form-control" placeholder="Votre adresse email" name="newsletter">
+        <div class="input-group-append">
+            <button class="btn btn-success" type="button">S'inscrire</button>
+        </div>
+    </div>
+    <div class="createur">Site réalisé par Guillaume Cazin.</div>
+    </div>
 </div>
